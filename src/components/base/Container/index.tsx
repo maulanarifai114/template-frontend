77e275702af528ab9baf817808ee055574ed9ff6
Raden Maulana Rifa'i Abdullah <maulanarifai114@gmail.com>
2024-10-31 23:33:48 +0700

    feat: Handle overflow on title

--- src/components/base/Container/index.tsx
+++ src/components/base/Container/index.tsx
@@ -9,7 +9,7 @@ export default function Container({ title, monospace, children }: { title: strin
   return (
     <div className="relative flex w-full flex-col items-start gap-4 border-b border-neutral-300 px-4 py-8">
       <div id={slug} className="absolute top-0"></div>
-      <div className="block w-full overflow-x-auto">
+      <div className={clsx({ "overflow-x-auto": monospace }, "block w-full")}>
         <Link href={`#${slug}`}>
           <h1 className={clsx({ "w-fit rounded-lg border border-neutral-200 bg-neutral-100 p-1 px-2 font-monospace": monospace })}>{title}</h1>
         </Link>
